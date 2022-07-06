4e9a2dd2b44065994811ea4896631214ed41631e
Nethaji-pandiarajan <ponram2503@gmail.com>
2022-07-06 19:43:32 +0530

    change index path

--- src/main/java/com/circuits99/servlet/Index.java
+++ src/main/java/com/circuits99/servlet/Index.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Index
  */
-@WebServlet("/index")
+@WebServlet("/")
 public class Index extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
